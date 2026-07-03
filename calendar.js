59408209e3a44fe96f849fd672c38029c80f5cc8
29miaoetlrsdnet <29miaoet@lrsd.net>
2026-07-02 20:29:02 -0500

    Began work on TypeScript main module

---
+++ calendar.js
@@ -0,0 +1,59 @@
+"use strict";
+class Calendar {
+    calendar;
+    dbPath;
+    startTime;
+    endTime;
+    now;
+    constructor(dbPath, startTime, endTime) {
+        this.dbPath = dbPath;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.now = Date.now();
+    }
+    async loadData() {
+        try {
+            const response = await fetch(this.dbPath);
+            if (!response.ok) {
+                throw new Error("Network response error " + response.statusText);
+            }
+            this.calendar = await response.json();
+        }
+        catch (error) {
+            console.error("There was a problem with the fetch operation:", error);
+        }
+    }
+    getRaw() {
+        console.log(this.calendar);
+    }
+    strftime(timeStamp) {
+        const date = new Date(timeStamp);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+    // Snapshot the current time to prevent mismatches between displays
+    snapshot() {
+        this.now = Date.now();
+    }
+    getAbsoluteTime() {
+        const timeObj = new Date(this.now);
+        const absoluteTime = {
+            days: timeObj.getDay(),
+            hours: timeObj.getHours(),
+            minutes: timeObj.getMinutes(),
+            seconds: timeObj.getSeconds()
+        };
+        console.log(absoluteTime);
+        return absoluteTime;
+    }
+}
+async function start() {
+    const cal = new Calendar("calendar.json", 0, 0);
+    await cal.loadData();
+    console.log(cal.strftime(Date.now()));
+    cal.getAbsoluteTime();
+    const id = setInterval(() => cal.getAbsoluteTime(), 1000);
+}
+start();
