b819c74e5e4ec63989dd76bd190b8349b8082ad0
29miaoetlrsdnet <29miaoet@lrsd.net>
2026-08-01 16:32:35 -0500

    Added handling for progress bar and statuses

--- src/dom.ts
+++ src/dom.ts
@@ -5,6 +5,9 @@ import Calendar from "./calendar";
 const schoolTimes = document.querySelectorAll<HTMLDivElement>(".school-time .timeunit .timebox");
 const totalTimes = document.querySelectorAll<HTMLDivElement>(".total-time .timeunit .timebox");
 const absoluteTimes = document.querySelectorAll<HTMLDivElement>(".abs-time .times .timebox");
+const dayStatuses = document.querySelectorAll<HTMLDivElement>(".day-info .day-card .card-content");
+const progressBar = document.getElementById<HTMLDivElement>("progress-bar-element");
+const progressText = document.getElementById<HTMLDivElement>("percentage");
 
 let schoolTimeRemaining: number;
 let totalTimeRemaining: number;
@@ -18,21 +21,23 @@ async function start() {
 
   await calendar.loadData();
 
-  updateTimer();
-  populateAbsoluteTimes(schoolTimeRemaining);
-  populateTotalTimes(totalTimeRemaining);
-  populateSchoolDates(schoolDates);
-
+  updateDOM();
   setInterval(() => {
-    updateTimer();
-    populateAbsoluteTimes(schoolTimeRemaining);
-    populateTotalTimes(totalTimeRemaining);
-    populateSchoolDates(schoolDates);
+    updateDOM();
   }, 100);
 }
 
 function updateDOM() {
+    updateTimer();
+
+    populateSchoolDates(schoolDates);
+    populateAbsoluteTimes(schoolTimeRemaining);
+
+    populateTotalTimes(totalTimeRemaining);
+    updateProgressBar();
+    updateDayInfos();
 }
+
 
 function updateTimer() {
   // calendar.freeze();
@@ -42,9 +47,16 @@ function updateTimer() {
   calendar.now = Date.now() + 2*31*24*60*60*1000 - 5*60*60*1000;
   // console.log(new Date(calendar.now));
 
-  schoolTimeRemaining = calendar.getSchoolTimeTo(tempdate);
+  try{
+    schoolTimeRemaining = calendar.getSchoolTimeTo(tempdate);
+    schoolDates = calendar.getSchoolTimeAsDate(calendar.strftime(tempdate));
+  } catch (Error) {
+    console.warn("Outside of calendar time frame, school time unavailable.");
+    schoolTimeRemaining = null;
+    schoolDates = null;
+  }
+
   totalTimeRemaining = calendar.getAbsoluteTimeTo(tempdate);
-  schoolDates = calendar.getSchoolTimeAsDate(calendar.strftime(tempdate));
 }
 
 
@@ -80,10 +92,43 @@ function populateTotalTimes(timeRemaining: number) {
 }
 
 function populateSchoolDates(schoolDates: Array<number>) {
-  for (let i=0; i<4; i++) {
+  for (let i = 0; i < 4; i++) {
     schoolTimes[i].textContent = schoolDates[i];
   }
 }
+
+function updateProgressBar() {
+  const start = new Date(2026, 5, 19, 15, 40);
+  const end = new Date(2026, 10, 9, 8, 30);
+  const fractionPercentage: number = calendar.getPercentCompletion(start.getTime(), end.getTime());
+  const percentFinished: string = `${fractionPercentage*100}%`;
+  progressBar.style.width = percentFinished;
+  progressText.textContent = percentFinished;
+}
+
+function updateDayInfos() {
+  const dayInfos = calendar.getDayInfo(calendar.strftime(calendar.now));
+  const daystatus = dayInfos.daystatus;
+
+  let feature;
+  if (dayInfos.feature.length !== 0) {
+    feature = "<p>" + dayInfos.feature.join("<br>") + "</p>";
+  } else {
+    feature = "<p>Nothing Interesting</p>";
+  }
+
+  let event;
+  if (dayInfos.event.length !== 0) {
+    event = "<p>" + dayInfos.event.join("<br>") + "</p>";
+  } else {
+    event = "<p>No Events</p>";
+  }
+
+  dayStatuses[0].textContent = daystatus;
+  dayStatuses[1].outerHTML = feature;
+  dayStatuses[2].outerHTML = event;
+}
+
 
 start();
 
